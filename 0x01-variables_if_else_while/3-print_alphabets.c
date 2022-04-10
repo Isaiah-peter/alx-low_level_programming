a42751150d54c4abc5d1ea89d4c5e20a2b7bb7ae
Isaiah peter <peterisaiah4fun@gmail.com>
2022-04-10 14:51:58 -0700

    if...else

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 char ch = 'a';
 char chA = 'A';
-for(ch = 'a'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-for(chA = 'A'; chA <= 'Z'; chA++)
+for (chA = 'A'; chA <= 'Z'; chA++)
 putchar(chA);
 printf("\n");
 return (0);
