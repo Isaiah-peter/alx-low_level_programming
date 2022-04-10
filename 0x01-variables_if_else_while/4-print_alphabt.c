598f27623144487e4325752e67ddf5cb57a06493
Isaiah peter <peterisaiah4fun@gmail.com>
2022-04-10 15:07:43 -0700

    if...else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Rteturn: 0 after running
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch == 'e');
+ch = "";
+if (ch == 'q');
+ch = "";
+putchar(ch);
+return (0);
+}
