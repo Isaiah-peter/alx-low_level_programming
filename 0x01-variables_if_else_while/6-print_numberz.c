a47bb75c07fa5feec4cb65203baf502de2396228
Isaiah peter <peterisaiah4fun@gmail.com>
2022-04-10 16:12:00 -0700

    if...else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - enter point
+ *
+ * Return: 0 after program run
+ */
+int main(void)
+{
+char ch = '0';
+for (ch = '0'; <= '9'; ch++)
+putchar(ch);
+printf("\n");
+return (0);
+}
