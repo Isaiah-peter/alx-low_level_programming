a3acef4cf9443873cddf508440383e457fc8e55a
Isaiah peter <peterisaiah4fun@gmail.com>
2022-04-14 15:11:02 -0700

    message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -20,7 +21,6 @@ i = 0;
  *putchar(i);
  *}
  */
-										  printf("Infinite loop avoided! \\o/\n");
-
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
