b140f41dd47e291246cdd9b8991c9defe6bf6d8e
Isaiah peter <peterisaiah4fun@gmail.com>
2022-04-15 15:42:08 -0700

    more

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9 and new number
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i);
+printf("\n");
+}
+}
