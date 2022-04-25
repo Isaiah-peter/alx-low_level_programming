652f6c171b524660f632b3db33370a150d2aa8a8
Isaiah peter <peterisaiah4fun@gmail.com>
2022-04-25 12:58:54 -0700

    more on string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * *_strncpy - copy
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+/**
+ */
+void reverse_array(int *a, int n)
+{
+int j;
+int i;
+j = n - 1;
+i = 0;
+for (i = 0; i < j; i++)
+{
+a[i] ^= a[j];
+a[j] ^= a[i];
+a[i] ^= a[j];
+j--;
+}
+}
