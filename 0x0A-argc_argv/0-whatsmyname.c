97446c41a7b0ce05a09d129cc30d3e6abe3dc3dd
Isaiah peter <peterisaiah4fun@gmail.com>
2022-05-02 15:11:31 -0700

    messge

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
  *Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-_putchar(argv[0]);
-_putchar('\n');
+printf("%s\n", argv[0]);
 return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  *Return: 0
  */
 
-int main (int argc, char *argv[])
+int main (int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc);
-return 0;
+return (0);
 }
