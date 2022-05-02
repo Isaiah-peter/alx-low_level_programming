1518e5d207b0316dd9dcfa406b03108ea73ee399
Isaiah peter <peterisaiah4fun@gmail.com>
2022-05-02 13:38:34 -0700

    more test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: int parameter
+ *@argv: char parameter
+ *Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+printf("%d\n", argc);
+return 0;
+}
