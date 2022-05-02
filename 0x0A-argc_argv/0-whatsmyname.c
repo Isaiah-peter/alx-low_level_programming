b531757cf4afca066c7e56a3321af3018e690b30
Isaiah peter <peterisaiah4fun@gmail.com>
2022-05-02 13:14:16 -0700

    more mesage

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - entry point
+ *@argc: int parameter
+ *@argv: char parameter
+ *Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+_putchar(argv[0]);
+return (0);
+}
