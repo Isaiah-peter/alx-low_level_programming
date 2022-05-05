d78446b9b4633034f7e235e38073d8184346cbc1
Isaiah peter <peterisaiah4fun@gmail.com>
2022-05-05 01:38:53 -0700

    more on malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * *_strdup - entry point
+ * @str: char parameter
+ * Return: char character
+ */
+
+char *_strdup(char *str)
+{
+int i;
+int len;
+char *s;
+
+len = strlen(str);
+s = malloc(len * sizeof(char));
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len; i++)
+{
+s[i] = str[i];
+}
+return (s);
+}
