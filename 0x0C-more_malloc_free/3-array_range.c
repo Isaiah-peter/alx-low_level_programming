5d931dfa71929063ae1e249d59587fa509d450ef
Isaiah peter <peterisaiah4fun@gmail.com>
2022-05-09 07:21:10 -0700

    hmmmmm

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -9,19 +8,23 @@
  */
 int *array_range(int min, int max)
 {
-	int *s;
-	int i;
+int *s;
+int i;
 
-	if (min > max)
-		return (NULL);
-	s = malloc(sizeof(int) * (max - min + 1));
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; min <= max; i++)
-	{
-		s[i] = min;
-		min++;
-	}
-	return (s);
+if (min > max)
+{
+return (NULL);
+}
+s = malloc(sizeof(int) * (max - min + 1));
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; min <= max; i++)
+{
+s[i] = min;
+min++;
+}
+return (s);
 
 }
