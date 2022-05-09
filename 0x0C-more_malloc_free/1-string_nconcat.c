490b08988b8038c62c7d302b8b8176c98cab1089
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-05-09 14:56:26 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,22 +3,37 @@
 #include <string.h>
 
 /**
- * *string_nconcat - entry point
- *@s1: char params
- *@s2: char params
- *@n: unsigned int
- *Return: void or memory or 98
+ * string_nconcat - concats 2 strings
+ * @s1: the first str
+ * @s2: the second str
+ * @n: how much of s2 to add
+ * Return: ptr to new string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s;
-s = malloc(n + sizeof(s1));
-if (s == NULL)
-{
-return (NULL);
-}
-strcat(s, s1);
-strncat(s, s2, n);
-return (s);
+	char *new;
+	int total = 0;
+	int i;
+	int j = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	total += strlen(s1) + n;
+	new = malloc((total * sizeof(char)) + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		new[i] = s1[i];
+	}
+	for (; n || !s2; n--, i++, j++)
+	{
+		new[i] = s2[j];
+	}
+	new[i] = '\0';
+	return (new);
 }
