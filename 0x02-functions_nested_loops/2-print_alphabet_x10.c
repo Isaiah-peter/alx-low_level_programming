bdd0b1f57890647f35aac975fd4d57cc332c88b8
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-06-20 18:45:17 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,36 +7,17 @@
   */
 void print_alphabet_x10(void)
 {
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int counter;
 int x;
 
 	while (x < 10)
 	{
-	_putchar('a');
-	_putchar('b');
-	_putchar('c');
-	_putchar('d');
-	_putchar('e');
-	_putchar('f');
-	_putchar('g');
-	_putchar('h');
-	_putchar('i');
-	_putchar('j');
-	_putchar('k');
-	_putchar('l');
-	_putchar('m');
-	_putchar('n');
-	_putchar('o');
-	_putchar('p');
-	_putchar('q');
-	_putchar('r');
-	_putchar('s');
-	_putchar('t');
-	_putchar('u');
-	_putchar('v');
-	_putchar('w');
-	_putchar('x');
-	_putchar('y');
-	_putchar('z');
+		for (counter = 0; counter < 26; counter++)
+
+		{
+			_putchar(alphabet[counter]);
+		}
 	_putchar('\n');
 	x++;
 	}
