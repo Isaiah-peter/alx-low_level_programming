b096600a6931657522818ad4eebc99a54d0de752
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-06-29 09:06:11 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,19 @@
 #include "main.h"
 /**
- * print_most_numbers - print int
- *
- * Retuen: Always 0 (sucess)
- */
-
+  * print_most_numbers - prints all the numbers except 2 and 4
+  *
+  *
+  * Return: void
+  */
 void print_most_numbers(void)
 {
 int i;
 
 for (i = 0; i < 10; i++)
 {
-
-if ( i == 2 || i == 4)
-
-i++;
-
-_putchar(i + '0');
+	if (i != 2 && i != 4)
+		_putchar(i + '0');
 
 }
 _putchar('\n');
-
 }
