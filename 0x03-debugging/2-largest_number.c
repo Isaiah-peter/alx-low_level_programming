3f01ba46f7500d7758b7d491586c1aa9b1d12157
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-09-15 15:01:33 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,15 +11,15 @@
 int largest_number(int a, int b, int c)
 {
 
-	if (a >= b && a >= c)
+	if(a >= b && a >= c)
 	{
 		return (a);
 	}
-	else if (b >= a && b >= c)
+	else if(b >= a && b >= c)
 	{
 		return (b);
 	}
-	else if (c >= b && c >= a)
+	else if(c >= b && c >= a)
 	{
 		return (c);
 	}
