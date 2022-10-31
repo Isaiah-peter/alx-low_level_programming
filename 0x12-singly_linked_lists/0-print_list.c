03ed7de1a2c1f4a8d27770a238993f5ecbf3ad84
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-31 08:28:01 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,17 @@
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
-
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-
+	
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		n = n + 1;
 		h = h->next;
 	}
