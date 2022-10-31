dc8fac3e0a532f90be7ee95658fb3145737e6c05
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-31 08:12:10 +0100

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* print_list - print link list element
+* @h: list parameter
+* Return: size of the link list
+*/
+
+size_t print_list(const list_t *h)
+{
+    size_t n = 0;
+
+    if (h->str == NULL)
+    {
+        printf("[0] (nil)\n");
+    }
+
+    while (h != NULL)
+	{
+        printf("[%d] %s\n", h->len, h->str);
+        n = n + 1;
+        h = h->next;
+    }
+    
+    return (n);
+}
