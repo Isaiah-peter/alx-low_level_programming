0e515c3f118da67cc2f5bfcc40bf39ef50316e72
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-10-31 09:17:33 +0100

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * *add_node - add to a linked list
+ * @head: list_t param
+ * @str: the name of the head->str
+ * Return: list_t
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_list = malloc(sizeof(list_t));
+
+    new_list->str = strdup(str);
+    new_list->len = strlen(str);
+    new_list->next = *head;
+    *head = new_list;
+
+    return (new_list);
+}
