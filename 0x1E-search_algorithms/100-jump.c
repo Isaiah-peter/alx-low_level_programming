e20a3b04da9189fea34adef0efc717d4ead33b82
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-11-02 11:22:43 +0100

    task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * jump_search - search algorithm
+ * @size: the number of elements in the array
+ * @value: the number to search
+ * Return: the index of the number
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t blockSize = sqrt(size);
+	size_t start = 0;
+	size_t end = blockSize;
+	size_t i;
+
+	printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+	while (array[end] < value && end < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", end, array[end]);
+		start = end;
+		end += blockSize;
+		if (end >= size)
+		{
+			end = size;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+
+	for(i = start; i < end; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
