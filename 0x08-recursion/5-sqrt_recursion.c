dfb51f4828e28908b443b3305d946ba5f4d2136a
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-16 01:15:39 +0100

    please work

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,36 @@
 #include <math.h>
 /**
- * _sqrt_recursion - get the square root of an integer
- * @n: int param
- * Return: int
- */
-
-int _sqrt_recursion(int n)
+  * issqr - find if it is a root
+  * @n1: number to check
+  * @n2: number base
+  * Return: square or -1
+**/
+int issqr(unsigned long n1, unsigned long n2)
 {
-int i;
-i = sqrt(n);
-if (i * i != n)
+if (n1 == n2)
+{ return (-1); }
+if (n1 * n1 == n2)
 {
+return (n1);
+}
+else
+{
+return (issqr((n1 + 1), n2));
+}
 return (-1);
 }
-
-return (i);
+/**
+* _sqrt_recursion - returns the natural square root
+* @n: number
+* Return: squre root
+**/
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+return (0);
+if (n < 0 || n == 2)
+return (-1);
+if (n == 1)
+return (1);
+return (issqr(2, n));
 }
